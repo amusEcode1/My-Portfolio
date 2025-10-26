3e0594fb46cbdb36e4c15bdf4ab26aa06c052f27
Ezekiel Oluyale <amusecode@gmail.com>
2025-10-26 12:50:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ if selected == "About Me":
         )
 
         # Use adaptive columns
-        col1, col2 = st.columns([1, 2], gap="small")
+        col1, col2 = st.columns([1, 2], gap="medium")
 
         with col1:
             st.markdown(
