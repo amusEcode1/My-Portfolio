8b20e135c36e8839c6fcb0777b7f799add9fd240
Ezekiel Oluyale <amusecode@gmail.com>
2025-10-26 12:26:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ button:hover {
 """, unsafe_allow_html=True)
 
 # SIDEBAR NAVIGATION 
-st.sidebar.image("logo.jpg", width=120)
+st.sidebar.image("logo.png", width=120)
 st.sidebar.title("Ezekiel Oluyale")
 st.sidebar.markdown("**NLP Researcher & Machine Learning Engineer**")
 
